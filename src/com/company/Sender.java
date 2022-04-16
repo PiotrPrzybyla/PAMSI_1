4b798229d2a5bfd80d7d746d389671c0f71c21f4
Piotr <pepapek@gmail.com>
2022-04-16 16:24:48 +0200

    Driver done

--- src/com/company/Sender.java
+++ src/com/company/Sender.java
@@ -1,4 +1,61 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Sender {
+    private String message;
+    private PackageList messageList;
+
+    public Sender() {
+        messageList = new PackageList();
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+
+    public PackageList getMessageList() {
+        return messageList;
+    }
+
+    public void setMessageList(PackageList messageList) {
+        this.messageList = messageList;
+    }
+
+
+    public void writeMessage(){
+        Scanner scanner = new Scanner(System.in);
+        message = scanner.nextLine();
+    }
+    public void divideMessage(int size) {
+
+        String tempMessage=message;
+        String partOfMessage;
+        int iterator = 1;
+        while(tempMessage!=null){
+            if(tempMessage.length()>size){
+
+                partOfMessage=tempMessage.substring(0, size);
+                tempMessage=tempMessage.substring(size);
+                messageList.insert(iterator, partOfMessage);
+                ++iterator;
+
+            } else{
+                messageList.insert(iterator++, tempMessage);
+                tempMessage=null;
+            }
+        }
+        if(iterator > 1){
+            System.out.println("Your message is too big. It will be sent in " + (iterator -1) + " packages");
+        }
+    }
+
+
+
 }
