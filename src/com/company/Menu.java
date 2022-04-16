fbd5f20c1bead2651eea86ef97fbd5245160f354
Piotr <pepapek@gmail.com>
2022-04-16 23:29:16 +0200

    Test Class implemented

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Menu {
     public Menu() {
         Sender sender = new Sender();
-        Recipent recipent = new Recipent();
+        Recipient recipient = new Recipient();
         Server server = new Server();
         Scanner scanner = new Scanner(System.in);
         int packageSize;
@@ -14,7 +14,7 @@ public class Menu {
         packageSize = scanner.nextInt();
         System.out.println("Write your Message: ");
         sender.writeMessage();
-        server.sendMessage(sender, recipent, packageSize);
+        server.sendMessage(sender, recipient, packageSize);
 
 
 
