94788968a26010f2623772dc61888569f7c4c508
Piotr <pepapek@gmail.com>
2022-04-22 20:19:57 +0200

    Driver fixed

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -14,6 +14,9 @@ public class Menu {
         packageSize = scanner.nextInt();
         System.out.println("Write your Message: ");
         sender.writeMessage();
+        System.out.println("Message is divided to packages, shuffled, and send to recipient");
+        System.out.println("Message is sorted and taken by the recipient. Sorting algorithm was:  ");
+        System.out.println("");
         server.sendMessage(sender, recipient, packageSize);
 
 
